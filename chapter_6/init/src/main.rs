7e65394f3aaefedcd2d9fc760d2660bac5e56035
Egor Fursov <egorf@live.ru>
2025-03-03 20:39:39 +0100

    wip chapter_6

---
+++ chapter_6/init/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"))
+
+    println!("Hello, world!");
+}
+
+enum IpAddrKind {
+    V4(String),
+    V6(String)
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+fn route(ip: IpAddrKind) {
+}
